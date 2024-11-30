1fc34731e539b6174ec6923a80adeae4304f36c2
Dinda <dindarrzki@gmail.com>
2024-12-01 00:07:06 +0700

    update 5

--- js/script.js
+++ js/script.js
@@ -20,27 +20,26 @@ function onFormSubmit(e) {
     const height = parseFloat(form.height.value);
 
     if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-        alert ("Please enter a valid weight and height");
+        alert ("Masukkan data yang valid");
         return;
     }
-
-    const heightInMeters = height / 100; //cm to m
+    const heightInMeters = height / 100; //cm -> m
     const bmi = weight / Math.pow(heightInMeters, 2);
     const desc = interpretBMI (bmi);
 
     bmiText.textContent = bmi.toFixed(2);
     bmiText.className = desc;
-    descText.innerHTML = 'Anda memiliki <strong> ${desc} </strong>';
+    descText.innerHTML = `Berat Badan Anda <strong>${desc}</strong>`;
 }
 
 function interpretBMI(bmi) {
     if (bmi < 18.5) {
-        return "underweight";
+        return "kurang";
     } else if (bmi < 25) {
-        return "healthy";
+        return "ideal";
     } else if (bmi < 30) {
-        return "overweight";
+        return "berlebih";
     } else {
-        return "obese";
+        return "obesitas";
     }
 }
